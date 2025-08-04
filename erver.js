890a1d20e09b04e2281b4fe01f1074baadf9db6c
CHI KIEN <your.email@example.com>
2025-08-04 13:40:47 +0700

    Delete erver.js

--- erver.js
+++
@@ -1,5 +0,0 @@
-[33me45b1ba[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Update config.js
-[33m129934b[m Fix Railway deployment: Update MongoDB connection and health check timeout
-[33m5249d35[m Update config.js
-[33md56015c[m Fix Vercel deployment: Remove API proxy and health check
-[33m85ecf7a[m Add Vercel deployment configuration and dynamic API URLs
